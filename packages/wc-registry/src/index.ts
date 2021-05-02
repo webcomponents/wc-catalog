c6ff7220f96ffecf70fc0e26a9dd8dd258e3f3f7
Justin Fagnani <justinfagnani@google.com>
2021-05-01 21:00:21 -0700

    Add graphql

--- packages/wc-registry/src/index.ts
+++ packages/wc-registry/src/index.ts
@@ -12,6 +12,21 @@ import firebase from 'firebase-admin';
 
 import {Firestore} from '@google-cloud/firestore';
 
+import {buildSchema} from 'graphql';
+import {readFile} from 'fs/promises';
+import {resolve, dirname} from 'path';
+import graphqlHTTP from 'koa-graphql';
+
+import type {Resolvers} from '../gen/resolvers';
+
+const base  = dirname(new URL(import.meta.url).pathname);
+const schemaSource = await readFile(resolve(base, './schema.graphql'), 'utf8');
+
+// console.log(schemaSource);
+
+const schema = buildSchema(schemaSource);
+// console.log(schema);
+
 firebase.initializeApp({
   projectId: 'wc-org',
 });
@@ -21,25 +36,43 @@ const db = new Firestore({
 });
 const docRef = db.collection('hello').doc('world');
 
+const resolvers: Resolvers = {
+  Query: {
+    count: async () => {
+      // This should work but gets stuck at 1:
+      // await docRef.set({count: FieldValue.increment(1)});
+      // const doc = await docRef.get();
+      // const count = doc.get('count');
+
+      // Racy increment:
+      const doc = await docRef.get();
+      const count = (doc.get('count') ?? 0) + 1;
+      await docRef.set({count});
+      return count;
+    },
+  },
+};
+
 const app = new Koa();
 
 const router = new Router();
+
+router.all('/graphql', graphqlHTTP({
+  schema,
+  rootValue: resolvers.Query,
+  graphiql: true,
+}));
+
 router.get('/', async (ctx) => {
-  // This should work but gets stuck at 1:
-  // await docRef.set({count: FieldValue.increment(1)});
-  // const doc = await docRef.get();
-  // const count = doc.get('count');
-
-  // Racy increment:
-  const doc = await docRef.get();
-  const count = (doc.get('count') ?? 0) + 1;
-  await docRef.set({count});
-
   ctx.status = 200;
   ctx.type = 'html';
   ctx.body = `
-    <h1>Hello</h1>
-    <p>This view has been loaded ${count} times.</p>
+    <h1>Web Components Registry</h1>
+    <p>
+      This server hosts a GraphQL API for interacting with the
+      web components registry.
+    </p>
+    <p>See the interactive query editor at <a href="/graphql">/graphql</a>.
   `;
 });
 
